2876c83692a73d01ebe09eb6278dca7c02d72dc2
Carlos Roberto Barbosa Junior <carlosjr.if@gmail.com>
2021-09-03 03:21:13 -0300

    [ADD] Scrapping inicial

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,6 +1,11 @@
 class QuotesController < ApplicationController
   before_action :set_quote, only: [:show, :update, :destroy]
 
+  require 'nokogiri'
+  require 'open-uri'
+
+  BASE_URL ="http://quotes.toscrape.com"
+
   # GET /quotes
   def index
     @quote = Quote.all
@@ -10,23 +15,53 @@ class QuotesController < ApplicationController
 
   # GET /quotes/:tag
   def search
-    searchTag = params[:tag]
-
-    if findDB(searchTag).length > 0
-
-    else
+    tag = params[:tag]
+    
+    if findTagDB(tag).length == 0
+      startCrawler(tag)
     end
 
-    render json: {quotes: @quotes.quotes} 
+    render json: { quotes: findTagDB(tag) } 
   end
 
-  def findDB(tag) 
-    Tag.where(tag)
-  end
+  private 
+    def findTagDB(tag) 
+      Tag.where(tag)
+    end
 
-  def resultFromDB(tag)
-  end
+    def startCrawler(tag)
+      url = "#{BASE_URL}/tag/#{tag}/"
+      begin
+        html = Nokogiri::HTML.parse(open(url))
+      rescue
+        return false
+      end
+      
+      quotes = html.css('.quote')
+      
+      
+      quotes.each do |quoteItem|
+        quote = Quote.new
+        
+        puts "#########################################"
+        #puts "#{quoteItem}"
+        quote.quote = quoteItem.css('.text').text
+        quote.author = quoteItem.css('.author').text
+        #aboutAuthor = 
+        tagsQuote = quoteItem.css('.tag')
+        quote.tags = []
+ 
+        tagsQuote.each do |tagQuote|
+          quote.tags << tagQuote.text
+        end
 
-  def crawlingTag
-  end
+        puts "#{quote.tags}"
+        puts "#########################################"
+      end
+
+    end
+
+    def resultFromDB(tag)
+      result
+    end
 end
